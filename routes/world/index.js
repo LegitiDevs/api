f7cf86ae845b861296b7c20396abb348cb7d4a97
Omri H <omri.hermon@gmail.com>
2024-12-04 17:26:12 +0200

    add world getter thing stuff to get world

--- routes/world/index.js
+++ routes/world/index.js
@@ -1,9 +1,17 @@
 "use strict";
+import "dotenv/config";
+import { MongoClient } from "mongodb";
 
-module.exports = async function (fastify, opts) {
-  fastify.get("/:world", async function (request, reply) {
-    const world = request.params.world;
+const MONGO_URI = process.env.MONGO_URI;
+const DB = process.env.DB;
+const mongoclient = new MongoClient(MONGO_URI);
+
+const worlds = mongoclient.db(DB).collection("worlds");
+
+export default async function (fastify, opts) {
+  fastify.get("/:uuid", async function (request, reply) {
+    const uuid = request.params.uuid;
+    const world = await worlds.findOne({ world_uuid: uuid });
     return world;
-    // TODO connect to db and world, then return it
   });
-};
+}
