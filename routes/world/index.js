a3d887e5c85fbeb9a9e70d83003c32593a6754b9
Omri H <omri.hermon@gmail.com>
2024-12-04 17:06:42 +0200

    oopsy evidence of me copy pasting :trol:

--- routes/world/index.js
+++ routes/world/index.js
@@ -4,6 +4,6 @@ module.exports = async function (fastify, opts) {
   fastify.get("/:world", async function (request, reply) {
     const world = request.params.world;
     return world;
-    // TODO connect to db and get x amount of top worlds, then return these
+    // TODO connect to db and world, then return it
   });
 };
