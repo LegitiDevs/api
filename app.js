ce3f6941455f4b1e866307eab908e8227dac0f3b
Omri H <omri.hermon@gmail.com>
2024-12-04 17:06:02 +0200

    yes i added ; to everything

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-'use strict'
+"use strict";
 
-const path = require('node:path')
-const AutoLoad = require('@fastify/autoload')
+const path = require("node:path");
+const AutoLoad = require("@fastify/autoload");
 
 // Pass --options via CLI arguments in command to enable these options.
-const options = {}
+const options = {};
 
 module.exports = async function (fastify, opts) {
   // Place here your custom code!
@@ -22,9 +22,9 @@ module.exports = async function (fastify, opts) {
   // This loads all plugins defined in routes
   // define your routes in one of these
   fastify.register(AutoLoad, {
-    dir: path.join(__dirname, 'routes'),
-    options: Object.assign({}, opts)
-  })
-}
+    dir: path.join(__dirname, "routes"),
+    options: Object.assign({}, opts),
+  });
+};
 
-module.exports.options = options
+module.exports.options = options;
