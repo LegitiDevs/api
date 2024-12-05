7455b3b12f7c5c30dd3f9aa684d5446148f435d8
PilkeySEK <156579268+PilkeySEK@users.noreply.github.com>
2024-12-05 08:00:22 +0100

    Add /top/max route

--- routes/top.js
+++
@@ -1,8 +0,0 @@
-'use strict'
-
-module.exports = async function (fastify, opts) {
-  fastify.get('/top/:max', async function (request, reply) {
-    const max = request.params.max;
-    // TODO connect to db and get x amount of top worlds, then return these
-  })
-}
