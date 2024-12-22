e7fc7e470f7334c84379696db5975e9f3486de07
Trplnr <87922281+Trioplane@users.noreply.github.com>
2024-12-22 13:27:39 +0800

    better searching + more sort options

--- routes/page/index.js
+++ routes/page/index.js
@@ -12,12 +12,19 @@ const PAGE_SIZE = 27; // How many worlds per page to return
 
 export default async function (fastify, opts) {
   fastify.get("/:index", async function (request, reply) {
-    console.log(request.query.sort, getSortingMethod(request.query.sort)) 
     const index = request.params.index;
     const skip = Number.parseInt(index) * PAGE_SIZE;
-    const sortingMethod = getSortingMethod(request.query.sort)
+    const sortingMethod = getSortingMethod(
+      request.query.sort,
+      request.query.sortDirection
+    );
 
-    const matched_worlds = await worlds.find().sort(sortingMethod).skip(skip).limit(PAGE_SIZE).toArray();
+    const matched_worlds = await worlds
+      .find()
+      .sort(sortingMethod)
+      .skip(skip)
+      .limit(PAGE_SIZE)
+      .toArray();
     return matched_worlds;
   });
 }
--- util/getSortingMethod.js
+++ util/getSortingMethod.js
@@ -1,13 +1,24 @@
 const SORTING_METHOD_LOOKUP = {
-    "default": { locked: -1, player_count: -1, votes: -1 }, // Online worlds first sorted by player count, then votes
-    "votes": { votes: -1 },
-    "visits": { visits: -1 },
-    "recently_scraped": { last_scraped: -1 },
+    ascending: {
+        "default": { locked: -1, player_count: -1, votes: -1 }, // Online worlds first sorted by player count, then votes
+        "votes": { votes: -1 },
+        "visits": { visits: -1 },
+        "recently_scraped": { last_scraped: -1 },
+        "recently_created": { creation_date_unix_time: -1 }
+    },
+    descending: {
+        "default": { locked: 1, player_count: 1, votes: 1 }, // Online worlds first sorted by player count, then votes
+        "votes": { votes: 1 },
+        "visits": { visits: 1 },
+        "recently_scraped": { last_scraped: 1 },
+        "recently_created": { creation_date_unix_time: 1 }
+    },
 }
 
-// ADD recently_added when creation_date_unix_time gets added.
+export const getSortingMethod = (sortMethod, direction) => {
+    const { ascending } = SORTING_METHOD_LOOKUP;
+    const directionLookup = SORTING_METHOD_LOOKUP[direction] || ascending;
+    return directionLookup[sortMethod] || directionLookup.default;
+};
 
-export const getSortingMethod = (sort) => {
-    if (!(sort in SORTING_METHOD_LOOKUP)) return SORTING_METHOD_LOOKUP["default"]
-    return SORTING_METHOD_LOOKUP[sort]
-}
+export const deRegexifyTheRegexSoTheUserDoesntDoMaliciousThings = input => input.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
