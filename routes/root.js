2a1d85b8e42b09f19b5e2367c530f2218fd23f60
Trplnr <87922281+Trioplane@users.noreply.github.com>
2024-12-24 17:31:23 +0800

    ehh scrap that

--- routes/root.js
+++ routes/root.js
@@ -1,6 +1,6 @@
 "use strict";
 
-import { version } from '../package.json' with { type: "json" }
+const VERSION = "1.1.2"
 
 export default async function (fastify, opts) {
   fastify.get("/", async function (request, reply) {
