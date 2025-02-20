c5243df206c7c96f23d832f6b6a876e6423feb76
Trplnr <87922281+Trioplane@users.noreply.github.com>
2025-02-21 06:39:25 +0800

    refactoring 2 and proper errors

--- routes/world/edit/index.js
+++ routes/world/edit/index.js
@@ -2,6 +2,8 @@
 import "dotenv/config";
 import { MongoClient } from "mongodb";
 import { canEditWorld } from "../../../util/utils.js";
+import { CONFIG } from "../../../util/config.js";
+import { BodyContentNotFoundError, BodyContentTooLongError, BodyContentWrongTypeError, DeniedWorldAccessError, WorldNotFoundError } from "../../../util/errors.js";
 
 const MONGO_URI = process.env.MONGO_URI;
 const DB = process.env.DB;
@@ -10,24 +12,44 @@ const mongoclient = new MongoClient(MONGO_URI);
 const worlds = mongoclient.db(DB).collection("worlds");
 
 export default async function (fastify, opts) {
-	fastify.post("/", async function (request, reply) {
-    const body = JSON.parse(request.body);
+	fastify.post("/description", async function (request, reply) {
+		const body = JSON.parse(request.body);
 		const world = await worlds.findOne({ world_uuid: body.world_uuid });
 
+		if (body.content == null)
+			return reply.send(new BodyContentNotFoundError())
+		if (typeof body.content !== "string")
+			return reply.send(new BodyContentWrongTypeError("string"))
+		if (body.content.length >= CONFIG.LEGITIDEVS.DESCRIPTION_LENGTH)
+			return reply.send(new BodyContentTooLongError(CONFIG.LEGITIDEVS.DESCRIPTION_LENGTH))
 		if (!world)
-			return reply
-				.code(404)
-				.header("Content-Type", "application/json; charset=utf-8")
-				.send({ message: `World '${body.world_uuid}' does not exist.` });
+			return reply.send(new WorldNotFoundError(body.world_uuid))
+		if (!(await canEditWorld(request.headers.authorization, world.owner_uuid))) 
+			return reply.send(new DeniedWorldAccessError(body.world_uuid))
 
-    if (!await canEditWorld(request.headers.authorization, world.owner_uuid)) {
-      return reply
-				.code(401)
-				.header("Content-Type", "application/json; charset=utf-8")
-				.send({ message: `You don't have access to edit this world.` });
-    }
+		// Passed security checks!
 
-    return reply.code(200).header("Content-Type", "application/json; charset=utf-8")
-				.send({ message: `go ahead` });
+		worlds.updateOne({ world_uuid: body.world_uuid }, { $set: { "legitidevs.description": body.content } })
+
+		return reply.code(200).send({ message: "Success." })
+	});
+
+	fastify.post("/unlist", async function (request, reply) {
+		const body = JSON.parse(request.body);
+		const world = await worlds.findOne({ world_uuid: body.world_uuid });
+
+		if (!world) 
+			return reply.send(new WorldNotFoundError(body.world_uuid));
+		if (!(await canEditWorld(request.headers.authorization, world.owner_uuid))) 
+			return reply.send(new DeniedWorldAccessError(body.world_uuid));
+
+		// Passed security checks!
+
+		worlds.updateOne(
+			{ world_uuid: body.world_uuid },
+			{ $set: { "legitidevs.unlisted": !world?.legitidevs?.unlisted } }
+		);
+
+		return reply.code(200).send({ message: "Success." });
 	});
 }
