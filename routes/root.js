a68bd2735a5e7c3bea46f9432e41b05ff942a6fc
Trplnr <87922281+Trioplane@users.noreply.github.com>
2025-04-24 13:21:47 +0800

    remove redundant status field on root route

--- routes/root.js
+++ routes/root.js
@@ -4,6 +4,6 @@ const VERSION = "3.2.0"
 
 export default async function (fastify, opts) {
   fastify.get("/", async function (request, reply) {
-    return { status: "ok", version: VERSION };
+    return { version: VERSION };
   });
 }
