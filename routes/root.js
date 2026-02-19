f4556e791965323fd2704ec4ddcea3135d577c0b
Omri H <omrih.e@protonmail.com>
2026-02-19 23:50:03 +0200

    feat: return scraper version

--- routes/root.js
+++ routes/root.js
@@ -1,9 +1,17 @@
 "use strict";
 
-const VERSION = "3.3.0"
+const VERSION = "3.4.0";
+const SCRAPER_URI = process.env.SCRAPER_URI;
 
 export default async function (fastify, opts) {
   fastify.get("/", async function (request, reply) {
-    return { version: VERSION };
+    var scraperData;
+    try {
+      const scraper = await fetch(SCRAPER_URI);
+      scraperData = await scraper.json();
+    } catch (e) {
+      scraperData = {};
+    }
+    return { version: VERSION, scraper: scraperData };
   });
 }
