a50252d018525fdae4719407d6406144930ec692
Omri H <omrih.e@protonmail.com>
2026-02-23 15:16:28 +0200

    feat: /stats endpoint

---
+++ routes/stats/index.js
@@ -0,0 +1,16 @@
+"use strict";
+import "dotenv/config";
+import { MongoClient } from "mongodb";
+
+const MONGO_URI = process.env.MONGO_URI;
+const DB = process.env.DB;
+const mongoclient = new MongoClient(MONGO_URI);
+
+const stats = mongoclient.db(DB).collection("stats");
+
+export default async function (fastify, opts) {
+  fastify.get("/", async function (request, reply) {
+    const all = await stats.find({}).toArray();
+    return all;
+  });
+}
