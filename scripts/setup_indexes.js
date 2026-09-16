f1e2473944c340b054a71fbaee53748711974f29
Trplnr <87922281+Trioplane@users.noreply.github.com>
2026-09-16 23:01:52 +0800

    Add DB scripts

---
+++ scripts/setup_indexes.js
@@ -0,0 +1,21 @@
+import "dotenv/config";
+import { MongoClient } from "mongodb";
+
+const MONGO_URI = process.env.MONGO_URI;
+const DB = process.env.DB;
+const mongoclient = new MongoClient(MONGO_URI);
+
+
+
+async function run() {
+	try {
+		const worlds = mongoclient.db(DB).collection("worlds");
+
+		const normalized_name_index = await worlds.createIndex({ normalized_name: "text" });
+		console.log(`Index created: ${normalized_name_index}`);
+	} finally {
+		await mongoclient.close();
+	}
+}
+
+run();
