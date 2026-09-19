7294a27dac3b2d35124633d64c2a70837defd9c0
Trplnr <87922281+Trioplane@users.noreply.github.com>
2026-09-19 13:42:38 +0800

    add message when scraper is unavailable

--- src/routes/root.ts
+++ src/routes/root.ts
@@ -2,7 +2,7 @@
 
 import { FastifyPluginAsyncTypebox } from "@fastify/type-provider-typebox";
 
-const VERSION = "4.0.0-alpha16";
+const VERSION = "4.0.0-alpha17";
 
 const plugin: FastifyPluginAsyncTypebox = async function (fastify, opts) {
   fastify.get("/", async function (request, reply) {
@@ -11,7 +11,7 @@ const plugin: FastifyPluginAsyncTypebox = async function (fastify, opts) {
       const scraper = await fastify.legitidevs_scraper.fetch('');
       scraperData = await scraper.json();
     } catch (e) {
-      scraperData = {};
+      scraperData = {_message: "The scraper is currently unavailable."};
     }
 
     return { version: VERSION, _message: "v3 is deprecated! Please try migrating to v4 immediately. v3 will be removed soon.", scraper: scraperData };
