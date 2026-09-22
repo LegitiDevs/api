5cb72dc210f587c9bec64acc830e25e5ab79cac4
Omri <omrih.e@protonmail.com>
2026-09-22 18:58:40 +0300

    fix: listen on 0.0.0.0

--- src/server.ts
+++ src/server.ts
@@ -48,7 +48,7 @@ closeWithGrace(
 );
 
 // Start listening.
-app.listen({ port: process.env.PORT || 3000 }, (err) => {
+app.listen({ port: process.env.PORT || 3000, host: "0.0.0.0" }, (err) => {
 	if (err) {
 		app.log.error(err);
 		process.exit(1);
